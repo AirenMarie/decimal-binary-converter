a99c82df7e9297055e4ef8c1e4944b5faf540367
AirenMarie <astringer92@gmail.com>
2024-06-29 18:28:07 -0400

    Create arrays in decimalToBinary

--- dec-bin.js
+++ dec-bin.js
@@ -2,7 +2,11 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => {};
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+};
 
 const checkUserInput = () => {
   if (
