ade34ac76fe6c1db9cb8074598e527a3e4c025ee
AirenMarie <astringer92@gmail.com>
2024-06-29 19:00:01 -0400

    Clean up decimalToBinary

--- dec-bin.js
+++ dec-bin.js
@@ -3,18 +3,18 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
 
   while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
